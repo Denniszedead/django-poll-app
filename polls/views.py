bd68a355069230f51c1ce20a121d026e6d4eb6e7
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-11-07 11:30:36 +0800

    For the poll index page, show the latest 5 questions

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Question
 
 def index(request):
-    latest_question_list = Question.objects.order_by("-publish-date")[:5]
+    latest_question_list = Question.objects.order_by("-publish_date")[:5]
     output = ", ".join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
 
