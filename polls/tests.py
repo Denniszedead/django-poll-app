80a68f81e4f781b3195692c2beb83fb051c96b99
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-11-27 14:34:45 +0800

    Add test_future_question test case

--- polls/tests.py
+++ polls/tests.py
@@ -96,3 +96,13 @@ class QuestionIndexViewTests(TestCase):
             [past_question2, past_question1]
         )
 
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future returns a 404 not found.
+        """
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
