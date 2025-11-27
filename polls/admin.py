a44f8207e1333a0abeb83e31450c1be90686a1f8
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-11-27 18:22:30 +0800

    Add publish_date filter

--- polls/admin.py
+++ polls/admin.py
@@ -8,6 +8,7 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ["question_text", "publish_date", "was_published_recently"]
+    list_filter = ["publish_date"]
     fieldsets = [
         (None, {"fields": ['question_text']}),
         ("Date information", {"fields": ["publish_date"], "classes": ["collapse"]})
