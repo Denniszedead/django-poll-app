b08179c3bf96c22a3781e12e22dace83300fd74a
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-11-27 12:30:26 +0800

    Add test_future_question method

--- polls/tests.py
+++ polls/tests.py
@@ -63,3 +63,12 @@ class QuestionIndexViewTests(TestCase):
             [question]
         )
 
+    def test_future_question(self):
+        """
+        Questions with a publish_date in the future aren't displayed on the index page
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
